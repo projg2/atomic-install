6225e7e38be708426057eac6821033dc845f96a6
Michał Górny <mgorny@gentoo.org>
2011-10-31 23:28:42 +0100

    Support traversing the tree to write a journal.

--- lib/journal.c
+++ lib/journal.c
@@ -8,14 +8,16 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <errno.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
 
 #ifdef HAVE_STDINT_H
 #	include <stdint.h>
-#endif
-
-#ifdef HAVE_LOCKF
-#	include <unistd.h>
 #endif
 
 #pragma pack(push)
@@ -34,10 +36,94 @@ struct ai_journal {
 
 #pragma pack(pop)
 
+static int ai_traverse_tree(const char *root, const char *path, FILE *outf, int is_dir) {
+	char *fn;
+	DIR *dir;
+	struct dirent *dent;
+	int ret;
+
+	fn = malloc(strlen(root) + strlen(path) + 2);
+	if (!fn)
+		return errno;
+	sprintf(fn, "%s/%s", root, path);
+
+	/* We need to check whether it's a directory */
+	if (!is_dir) {
+		struct stat st;
+
+		if (lstat(fn, &st)) {
+			free(fn);
+			return errno;
+		}
+
+		if (!S_ISDIR(st.st_mode)) {
+			free(fn);
+			return ENOTDIR;
+		}
+	}
+
+	dir = opendir(fn);
+	free(fn);
+	if (!dir)
+		return errno;
+
+	errno = 0;
+	while ((dent = readdir(dir))) {
+		int len, ret;
+		int is_dir = -1;
+
+		/* Omit . & .. */
+		if (dent->d_name[0] == '.' && (!dent->d_name[1]
+					|| (dent->d_name[1] == '.' && !dent->d_name[2])))
+			continue;
+
+#ifdef DT_DIR
+		/* It's awesome if we can avoid falling back to stat() */
+		if (dent->d_type == DT_DIR)
+			is_dir = 1;
+		else if (dent->d_type != DT_UNKNOWN)
+			is_dir = 0;
+#endif
+
+		/* Prepare the relative path */
+		len = strlen(path) + strlen(dent->d_name) + 2;
+		fn = malloc(len);
+		if (!fn)
+			break;
+		sprintf(fn, "%s/%s", path, dent->d_name);
+
+		if (is_dir != 0) {
+			ret = ai_traverse_tree(root, fn, outf, is_dir == 1);
+
+			if (ret == ENOTDIR)
+				is_dir = 0;
+		}
+
+		if (!is_dir) {
+			if (fwrite(fn, len, 1, outf) != 1)
+				ret = errno;
+		}
+
+		free(fn);
+		if (ret) {
+			closedir(dir);
+			return ret;
+		}
+
+		errno = 0;
+	}
+	ret = errno;
+
+	if (closedir(dir) && !ret)
+		ret = errno;
+	return ret;
+}
+
 int ai_journal_create(const char *journal_path, const char *location) {
 	struct ai_journal newj = { "AIj!", 0x0000, 0, 0 };
 
 	FILE *f;
+	int ret;
 #ifdef HAVE_LOCKF
 	int fd;
 #endif
@@ -56,13 +142,13 @@ int ai_journal_create(const char *journal_path, const char *location) {
 		return errno;
 	}
 
-	/* XXX */
+	ret = ai_traverse_tree(location, "", f, 1);
 
 #ifdef HAVE_LOCKF
 	lockf(fd, F_ULOCK, 0);
 #endif
-	if (fclose(f))
-		return errno;
+	if (fclose(f) && !ret)
+		ret = errno;
 
-	return 0;
+	return ret;
 }
