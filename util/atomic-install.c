c33a302fc7d38f36b53afa2abbd1823f89b09828
Michał Górny <mgorny@gentoo.org>
2011-11-11 16:24:31 +0100

    Split the main loop into a separate function.

--- util/atomic-install.c
+++ util/atomic-install.c
@@ -51,34 +51,124 @@ static void print_progress(const char *path, unsigned long int megs, unsigned lo
 		printf(">>> %s\n", path);
 }
 
-int main(int argc, char *argv[]) {
+struct loop_data {
 	ai_journal_t j;
+
+	const char *source;
+	const char *dest;
+	const char *journal_file;
+
+	int rollback;
+	int noreplace;
+	int verbose;
+	int onestep;
+};
+
+static int loop(struct loop_data *d) {
+	int ret;
+
+	while (1) {
+		const uint32_t flags = ai_journal_get_flags(d->j);
+
+		if (flags & AI_MERGE_ROLLBACK_STARTED || d->rollback) {
+			/* Proceed with rollback. */
+			if (flags & AI_MERGE_REPLACED) {
+				printf("! Replacement complete, rollback impossible.\n");
+				ret = 1;
+				break;
+			} else if (flags & AI_MERGE_BACKED_OLD_UP) {
+				printf("* Rolling back replacement...\n");
+				ret = ai_merge_rollback_replace(d->dest, d->j);
+				if (ret) {
+					printf("* Replacement rollback failed: %s\n", strerror(ret));
+					break;
+				}
+			} else {
+				printf("* Rolling back old backup...\n");
+				ret = ai_merge_rollback_old(d->dest, d->j);
+				if (ret) {
+					printf("* Old rollback failed: %s\n", strerror(ret));
+					break;
+				}
+			}
+			printf("* Rolling back new copying...\n");
+			ret = ai_merge_rollback_new(d->dest, d->j);
+			if (ret)
+				printf("* New rollback failed: %s\n", strerror(ret));
+			else {
+				printf("* Rollback successful.\n");
+				if (unlink(d->journal_file))
+					printf("Journal removal failed: %s\n", strerror(errno));
+			}
+			break;
+		} else if (flags & AI_MERGE_REPLACED) {
+			printf("* Post-merge clean up...\n");
+			ret = ai_merge_cleanup(d->dest, d->j);
+			if (ret)
+				printf("Cleanup failed: %s\n", strerror(ret));
+			else {
+				printf("* Install done.\n");
+				if (unlink(d->journal_file))
+					printf("Journal removal failed: %s\n", strerror(errno));
+			}
+			break;
+		} else if (flags & AI_MERGE_BACKED_OLD_UP && flags & AI_MERGE_COPIED_NEW) {
+			if (d->noreplace)
+				break;
+			printf("* Replacing files...\n");
+			ret = ai_merge_replace(d->dest, d->j);
+			if (ret) {
+				printf("Replacement failed: %s\n", strerror(ret));
+				d->rollback = 1;
+			}
+		} else if (flags & AI_MERGE_COPIED_NEW) {
+			printf("* Backing up existing files...\n");
+			ret = ai_merge_backup_old(d->dest, d->j);
+			if (ret) {
+				printf("Backing old up failed: %s\n", strerror(ret));
+				break;
+			}
+		} else {
+			printf("* Copying new files...\n");
+			ret = ai_merge_copy_new(d->source, d->dest, d->j,
+					d->verbose ? print_progress : NULL);
+			if (ret) {
+				printf("Copying new failed: %s\n", strerror(ret));
+				break;
+			}
+		}
+
+		if (d->onestep)
+			break;
+	}
+
+	return ret;
+}
+
+int main(int argc, char *argv[]) {
 	int opt;
 	int ret, ret2;
 
-	const char *journal_file, *source, *dest;
-	int onestep = 0;
+	struct loop_data d;
+
 	int resume = 0;
-	int rollback = 0;
-	int noreplace = 0;
-	int verbose = 0;
 
 	while ((opt = getopt_long(argc, argv, "hV1nrRv", opts, NULL)) != -1) {
 		switch (opt) {
 			case '1':
-				onestep = 1;
+				d.onestep = 1;
 				break;
 			case 'n':
-				noreplace = 1;
+				d.noreplace = 1;
 				break;
 			case 'r':
 				resume = 1;
 				break;
 			case 'R':
-				rollback = 1;
+				d.rollback = 1;
 				break;
 			case 'v':
-				verbose = 1;
+				d.verbose = 1;
 				break;
 			case 'V':
 				printf("%s\n", PACKAGE_STRING);
@@ -96,107 +186,35 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 
-	journal_file = argv[optind];
-	source = argv[optind + 1];
-	dest = argv[optind + 2];
+	d.journal_file = argv[optind];
+	d.source = argv[optind + 1];
+	d.dest = argv[optind + 2];
 
 	/* Try to open.
 	 * If it doesn't exist, try to create and then open. */
-	ret = ai_journal_open(journal_file, &j);
+	ret = ai_journal_open(d.journal_file, &d.j);
 	if (!ret)
 		printf("* Journal file open, %s.\n",
-				rollback ? "rolling back" : "resuming");
-	else if (ret == ENOENT && !resume && !rollback) {
+				d.rollback ? "rolling back" : "resuming");
+	else if (ret == ENOENT && !resume && !d.rollback) {
 		printf("* Journal not found, creating...\n");
 
-		ret = ai_journal_create(journal_file, source);
+		ret = ai_journal_create(d.journal_file, d.source);
 		if (ret) {
 			printf("Journal creation failed: %s\n", strerror(ret));
 			return ret;
 		}
 
-		ret = ai_journal_open(journal_file, &j);
+		ret = ai_journal_open(d.journal_file, &d.j);
 	}
 	if (ret) {
 		printf("Journal open failed: %s\n", strerror(ret));
 		return ret;
 	}
 
-	while (1) {
-		const uint32_t flags = ai_journal_get_flags(j);
+	ret = loop(&d);
 
-		if (flags & AI_MERGE_ROLLBACK_STARTED || rollback) {
-			/* Proceed with rollback. */
-			if (flags & AI_MERGE_REPLACED) {
-				printf("! Replacement complete, rollback impossible.\n");
-				ret = 1;
-				break;
-			} else if (flags & AI_MERGE_BACKED_OLD_UP) {
-				printf("* Rolling back replacement...\n");
-				ret = ai_merge_rollback_replace(dest, j);
-				if (ret) {
-					printf("* Replacement rollback failed: %s\n", strerror(ret));
-					break;
-				}
-			} else {
-				printf("* Rolling back old backup...\n");
-				ret = ai_merge_rollback_old(dest, j);
-				if (ret) {
-					printf("* Old rollback failed: %s\n", strerror(ret));
-					break;
-				}
-			}
-			printf("* Rolling back new copying...\n");
-			ret = ai_merge_rollback_new(dest, j);
-			if (ret)
-				printf("* New rollback failed: %s\n", strerror(ret));
-			else {
-				printf("* Rollback successful.\n");
-				if (unlink(journal_file))
-					printf("Journal removal failed: %s\n", strerror(errno));
-			}
-			break;
-		} else if (flags & AI_MERGE_REPLACED) {
-			printf("* Post-merge clean up...\n");
-			ret = ai_merge_cleanup(dest, j);
-			if (ret)
-				printf("Cleanup failed: %s\n", strerror(ret));
-			else {
-				printf("* Install done.\n");
-				if (unlink(journal_file))
-					printf("Journal removal failed: %s\n", strerror(errno));
-			}
-			break;
-		} else if (flags & AI_MERGE_BACKED_OLD_UP && flags & AI_MERGE_COPIED_NEW) {
-			if (noreplace)
-				break;
-			printf("* Replacing files...\n");
-			ret = ai_merge_replace(dest, j);
-			if (ret) {
-				printf("Replacement failed: %s\n", strerror(ret));
-				ai_journal_set_flag(j, AI_MERGE_ROLLBACK_STARTED);
-			}
-		} else if (flags & AI_MERGE_COPIED_NEW) {
-			printf("* Backing up existing files...\n");
-			ret = ai_merge_backup_old(dest, j);
-			if (ret) {
-				printf("Backing old up failed: %s\n", strerror(ret));
-				break;
-			}
-		} else {
-			printf("* Copying new files...\n");
-			ret = ai_merge_copy_new(source, dest, j, verbose ? print_progress : NULL);
-			if (ret) {
-				printf("Copying new failed: %s\n", strerror(ret));
-				break;
-			}
-		}
-
-		if (onestep)
-			break;
-	}
-
-	ret2 = ai_journal_close(j);
+	ret2 = ai_journal_close(d.j);
 	if (ret2)
 		printf("Journal close failed: %s\n", strerror(ret));
 
